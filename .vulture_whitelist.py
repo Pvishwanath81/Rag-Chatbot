45d5c004b00d36e9e55ddcd82aea5a24fd51656b
P Vishwanath <vishwanathpeddanolla@gmail.com>
2026-06-25 13:01:51 +0530

    Update vulture whitelist

--- .vulture_whitelist.py
+++ .vulture_whitelist.py
@@ -2,10 +2,8 @@
 """Whitelist for vulture dead code detection."""
 
 import streamlit as st
-
-_ = st.session_state.pending_uploads
-
 from utils.pdf_loader import load_pdf_from_upload, get_document_metadata
 
+_ = st.session_state.pending_uploads
 _ = load_pdf_from_upload
 _ = get_document_metadata
